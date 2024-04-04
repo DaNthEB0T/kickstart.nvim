f019d66a6fa2c5d86bb63487de145508ca9447cd
Daniel Kamenov <sonnatherman04@gmail.com>
2024-04-04 21:05:14 +0200

    LSPs, autoformatting, configuration

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -193,6 +193,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+require 'custom.autocmds'
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
@@ -538,9 +539,10 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        basedpyright = {},
+        black = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -836,7 +838,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
---
+++ lua/custom/autocmds.lua
@@ -0,0 +1,35 @@
+local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+
+local autocmd_group = augroup('Auto-formatters', { clear = true })
+
+autocmd({ 'BufWritePost' }, {
+  pattern = { '*.py' },
+  desc = 'Auto-format Python files on save',
+  callback = function()
+    local fileName = vim.api.nvim_buf_get_name(0)
+    vim.cmd(':silent !black --preview -q ' .. fileName)
+    vim.cmd(':silent !isort --profile black --float-to-top -q ' .. fileName)
+    vim.cmd(':silent !docformatter --in-place --black ' .. fileName)
+  end,
+  group = autocmd_group,
+})
+autocmd({ 'BufWritePost' }, {
+  pattern = { '*.c', '*.h' },
+  desc = 'Auto-format C source/header files on save',
+  callback = function()
+    local filename = vim.api.nvim_buf_get_name(0)
+    vim.cmd(':silent !clang-format -i ' .. filename)
+  end,
+})
+
+local autoneotree_group = augroup('Neotree', {})
+autocmd({ 'UiEnter' }, {
+  desc = 'Automatically open Neotree on Vim Startup',
+  callback = function()
+    if vim.fn.argc() == 0 then
+      vim.cmd 'Neotree toggle'
+    end
+  end,
+  group = autoneotree_group,
+})
