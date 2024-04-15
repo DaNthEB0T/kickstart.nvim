35ce68fedb5925f90632c824a7cebc41bfdb9ba2
Daniel Kamenov <sonnatherman04@gmail.com>
2024-04-15 04:36:30 +0200

    Change Neotree default width

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -6,7 +6,7 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  config = function()
-    require('neo-tree').setup {}
-  end,
+  opts = { window = {
+    width = 30,
+  } },
 }
